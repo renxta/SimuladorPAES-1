dcf0827784ef22924cfba6099e0e8e15c35d3e23
Renata <renataquinterosorozco@gmail.com>
2025-11-03 22:26:43 -0300

    Corrijo import para FastAPI en Railway

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from app import simulador
+from backend.app import simulador
 import os
 
 app = FastAPI(
