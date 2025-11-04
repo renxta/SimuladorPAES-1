46649839312ea10948cfd9e793c4eeca3c885d84
Renata <renataquinterosorozco@gmail.com>
2025-11-03 23:37:03 -0300

    importaciones

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DECIMAL, ForeignKey
 from sqlalchemy.orm import relationship
-from db import Base
+from .db import Base
 
 # =======================
 #   Universidades
